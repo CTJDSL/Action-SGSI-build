04242790c1a89225d9f9d5b57b2359aceaf20b65
冲田酱大胜利 <82740390+fghxhjj@users.noreply.github.com>
2022-08-23 09:26:34 +0800

    Update h2os.sh

--- apps_clean/h2os.sh
+++ apps_clean/h2os.sh
@@ -2,4 +2,14 @@
 
 LOCALDIR=`cd "$( dirname $0 )" && pwd`
 cd $LOCALDIR
+systemdir=$1
 
+apps_dir="
+
+"
+
+for delete_dir in $apps_dir ;do
+  find $systemdir -type d -name "$delete_dir" | xargs rm -rf
+done
+
+rm -rf $1/data-app/*
